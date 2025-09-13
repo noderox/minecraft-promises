a315aefce9b9ce4f1d03b6ea3397299e3d22c15f
Mcarby <sifddin@hotmail.com>
2025-09-13 17:16:30 +0200

    ✨:  added constants file

---
+++ src/config/constants.js
@@ -0,0 +1,21 @@
+export const BLOCK = {
+    stone:"🪨",
+    diamond:"💎",
+    dirt: "🟧"
+};
+
+
+export const CROPS ={
+carret: "🥕",
+wheat: "🌾"
+}
+
+export const TOOLS = {
+pickaxe: "⛏️",
+hoe: "🪓",
+sword:"⚔️"
+}
+
+export const TRADES = {
+diamond: "1 💎 ➡️  3 🍞"
+}
